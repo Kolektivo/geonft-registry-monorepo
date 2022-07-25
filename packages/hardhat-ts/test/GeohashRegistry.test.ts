0e53b40c53c9db3dcadf0fb4f18d1fe09eeb87d2
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-07-25 13:01:43 +0200

    improve geohash tests

--- packages/hardhat-ts/test/GeohashRegistry.test.ts
+++ packages/hardhat-ts/test/GeohashRegistry.test.ts
@@ -8,7 +8,9 @@ import {
 } from "../typechain";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 // eslint-disable-next-line node/no-missing-import
-import { solidityCoordinate } from "../utils/geomUtils";
+import { solidityCoordinate, centroid } from "../utils/geomUtils";
+// eslint-disable-next-line node/no-missing-import
+import { GEOJSON2 } from "./SDRegistry.mock";
 
 const { expect } = chai;
 
@@ -208,79 +210,84 @@ describe("geohash registry", () => {
 
   describe("Geohash encoding", async () => {
     // Reference: https://www.movable-type.co.uk/scripts/geohash.html
-    it("Encodes coordinates with precision 1", async () => {
+    it("Encodes Curazao's coordinates with precision from 1 to 9", async () => {
+      const expectedGeohash = "d6nvms58e"; // Precision 9
       const lat = 12.194946;
       const lon = -69.011151;
-      const precision = 1;
-
       const latSol = solidityCoordinate(lat); // Solidity latitude version
       const lonSol = solidityCoordinate(lon); // Solidity longitude version
-      const geohash = await geohashRegistry.encode(latSol, lonSol, precision);
 
-      expect(geohash).to.equal("d");
+      // Array of promises with the geohash returned from the encode() function.
+      // Each geohash is calculated using the same coordinates pair but with
+      // a different precision, withing a range from 1 to 9
+      const geohashEncodingPromises: Array<Promise<string>> = Array.from({
+        length: 9,
+      }).map((_, i: number) => {
+        const precision = i + 1;
+        return geohashRegistry.encode(latSol, lonSol, precision);
+      });
+      const geohashes = await Promise.all(geohashEncodingPromises);
+
+      geohashes.forEach((geohash, i) => {
+        // Calculate expected geohash for certain precision i by slicing the
+        // full expectedGeohash. So, the expected geohash for precision 4
+        // would be: "d6nv" (length = 4)
+        const expectedGeohashForPrecisionI = expectedGeohash.slice(0, i + 1);
+        expect(geohash).to.equal(expectedGeohashForPrecisionI);
+      });
     });
 
-    it("Encodes coordinates with precision 4", async () => {
-      const lat = 12.194946;
-      const lon = -69.011151;
-      const precision = 4;
-
+    it("Encodes Curazao's inverted coordinates with precision from 1 to 9", async () => {
+      const expectedGeohash = "mtc4d7urk"; // Precision 9
+      const lat = -12.194946;
+      const lon = 69.011151;
       const latSol = solidityCoordinate(lat); // Solidity latitude version
       const lonSol = solidityCoordinate(lon); // Solidity longitude version
-      const geohash = await geohashRegistry.encode(latSol, lonSol, precision);
 
-      expect(geohash).to.equal("d6nv");
+      // Array of promises with the geohash returned from the encode() function.
+      // Each geohash is calculated using the same coordinates pair but with
+      // a different precision, withing a range from 1 to 9
+      const geohashEncodingPromises: Array<Promise<string>> = Array.from({
+        length: 9,
+      }).map((_, i: number) => {
+        const precision = i + 1;
+        return geohashRegistry.encode(latSol, lonSol, precision);
+      });
+      const geohashes = await Promise.all(geohashEncodingPromises);
+
+      geohashes.forEach((geohash, i) => {
+        // Calculate expected geohash for certain precision i by slicing the
+        // full expectedGeohash. So, the expected geohash for precision 4
+        // would be: "d6nv" (length = 4)
+        const expectedGeohashForPrecisionI = expectedGeohash.slice(0, i + 1);
+        expect(geohash).to.equal(expectedGeohashForPrecisionI);
+      });
     });
 
-    it("Encodes coordinates with precision 6", async () => {
-      const lat = 12.194946;
-      const lon = -69.011151;
-      const precision = 6;
-
-      const latSol = solidityCoordinate(lat); // Solidity latitude version
-      const lonSol = solidityCoordinate(lon); // Solidity longitude version
-      const geohash = await geohashRegistry.encode(latSol, lonSol, precision);
-
-      expect(geohash).to.equal("d6nvms");
-    });
-
-    it("Encodes coordinates with precision 8", async () => {
-      const lat = 12.194946;
-      const lon = -69.011151;
+    it("Encodes GeoJSON 2 polygon with precision 8", async () => {
+      const geometry = GEOJSON2.features[0].geometry;
+      const [lat, lon] = centroid(geometry);
       const precision = 8;
-
       const latSol = solidityCoordinate(lat); // Solidity latitude version
       const lonSol = solidityCoordinate(lon); // Solidity longitude version
+
       const geohash = await geohashRegistry.encode(latSol, lonSol, precision);
 
-      expect(geohash).to.equal("d6nvms58");
-    });
-
-    it("Encodes coordinates with precision 9", async () => {
-      const lat = 12.194946;
-      const lon = -69.011151;
-      const precision = 9;
-
-      const latSol = solidityCoordinate(lat); // Solidity latitude version
-      const lonSol = solidityCoordinate(lon); // Solidity longitude version
-      const geohash = await geohashRegistry.encode(latSol, lonSol, precision);
-
-      expect(geohash).to.equal("d6nvms58e");
+      expect(geohash).to.equal("d6pj0777");
     });
   });
 
   describe("Geohash decoding", async () => {
     it("Decodes geohash with precision 8", async () => {
       const geohash = "d6nvms58";
-      const latExpected = 12194910048; // Solidity latitude version with a little error deviation
-      const lonExpected = -69011135100; // Solidity longitude version with a little error devaition
+      // Solidity coordinates version with a little error deviation
+      const coordinatesExpected = [12194910048, -69011135100];
 
       const coordinatesBigNumber = await geohashRegistry.decode(geohash);
       // Transform from BigNumber hex value to number
-      const [lat, lon] = coordinatesBigNumber.map((coord) => coord.toNumber());
+      const coordinates = coordinatesBigNumber.map((coord) => coord.toNumber());
 
-      expect(lat).to.equal(latExpected);
-      expect(lon).to.equal(lonExpected);
+      expect(coordinates).deep.equal(coordinatesExpected);
     });
   });
 });
