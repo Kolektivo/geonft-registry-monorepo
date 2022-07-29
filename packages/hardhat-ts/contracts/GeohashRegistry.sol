586eace251e161a51a12b949c25b463e833f314b
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-07-29 13:59:15 +0200

    fix linter error

--- packages/hardhat-ts/contracts/GeohashRegistry.sol
+++ packages/hardhat-ts/contracts/GeohashRegistry.sol
@@ -8,10 +8,10 @@ contract GeohashRegistry {
     // length of the geohash string
     uint8 public constant GEOHASH_LENGTH = 8;
     uint256 private constant COORD_EXP = 1e9; // Coordinates exponent to avoid decimals
-    int64 private constant MIN_LAT = -90*int64(int(COORD_EXP));
-    int64 private constant MAX_LAT = 90*int64(int(COORD_EXP));
-    int64 private constant MIN_LON = -180*int64(int(COORD_EXP));
-    int64 private constant MAX_LON = 180*int64(int(COORD_EXP));
+    int64 private constant MIN_LAT = -90 * int64(int256(COORD_EXP));
+    int64 private constant MAX_LAT = 90 * int64(int256(COORD_EXP));
+    int64 private constant MIN_LON = -180 * int64(int256(COORD_EXP));
+    int64 private constant MAX_LON = 180 * int64(int256(COORD_EXP));
     string private constant BASE32_CODES = "0123456789bcdefghjkmnpqrstuvwxyz"; // Codes for geohash encoding
     bytes private constant CODES_BYTES = bytes(BASE32_CODES); // Codes in bytes format to iterate as array
 
@@ -38,7 +38,11 @@ contract GeohashRegistry {
      * @param _lon longitude
      * @param _data GeoNFT ID
      */
-    function register(int64 _lat, int64 _lon, uint256 _data) public {
+    function register(
+        int64 _lat,
+        int64 _lon,
+        uint256 _data
+    ) public {
         string memory geohash = encode(_lat, _lon, GEOHASH_LENGTH);
         this.add(geohash, _data);
     }
@@ -47,7 +51,11 @@ contract GeohashRegistry {
      * @notice Get a data by geohash
      * @param _geohash the geohash
      */
-    function get(string memory _geohash) public view returns (uint256[] memory) {
+    function get(string memory _geohash)
+        public
+        view
+        returns (uint256[] memory)
+    {
         Node storage node = nodes[_geohash];
         return node.data;
     }
@@ -61,16 +69,18 @@ contract GeohashRegistry {
         // require the length of the _geohash is GEOHASH_LENGTH
         require(bytes(_geohash).length == GEOHASH_LENGTH);
 
+        // geohash characters splitted into an array
         bytes calldata geohashArray = bytes(_geohash);
         // geohash substring used each level of the subtree
-        string memory subhash;
+        // string memory subhash;
 
         // iterates over all 8 levels of the geohash and insert/append data
         // to each of those levels indexed by its subhash
-        for (uint8 i = 0; i < bytes(_geohash).length; i++) {
-            // create subhash according to the depth level by slicing original geohash
-            // subhash of 'gc7j98fg' at level 3 would be -> 'gc7'
-            subhash = string(geohashArray[:i + 1]);
+        for (uint8 i = 0; i < geohashArray.length; i++) {
+            // create subhash according to the depth level by slicing original geohash;
+            // subhash of 'gc7j98fg' at level 3 would be -> 'gc7';
+            // solhint-disable-next-line
+            string memory subhash = string(geohashArray[0:i + 1]);
 
             // lookup existing node
             Node storage node = nodes[subhash];
@@ -107,7 +117,11 @@ contract GeohashRegistry {
      * @param _newgeohash the new geohash
      * @param _data the uint data
      */
-    function update(string memory _formergeohash, string calldata _newgeohash, uint256 _data) public {
+    function update(
+        string memory _formergeohash,
+        string calldata _newgeohash,
+        uint256 _data
+    ) public {
         // remove data from former geohash
         remove(_formergeohash, _data);
 
@@ -156,7 +170,11 @@ contract GeohashRegistry {
      * @param _node node
      * @param _data the uint data
      */
-    function dataExists(Node memory _node, uint256 _data) internal pure returns (bool) {
+    function dataExists(Node memory _node, uint256 _data)
+        internal
+        pure
+        returns (bool)
+    {
         for (uint256 i = 0; i < _node.data.length; i++) {
             if (_node.data[i] == _data) {
                 return true;
@@ -173,7 +191,11 @@ contract GeohashRegistry {
      * @param _precision geohash length precision
      * @return Geohash string with the same length as the _precision value (1-9)
      */
-    function encode(int64 _lat, int64 _lon, uint8 _precision) public pure returns (string memory) {
+    function encode(
+        int64 _lat,
+        int64 _lon,
+        uint8 _precision
+    ) public pure returns (string memory) {
         bytes memory hashBytes = new bytes(_precision);
         int8 bits = 0;
         int8 bitsTotal = 0;
@@ -229,7 +251,11 @@ contract GeohashRegistry {
      * @param _geohash Geohash string up to 9 characters of precision
      * @return Array of Big Number integer coordinates (lat, lon)
      */
-    function decode(string memory _geohash) public view returns (int64[2] memory) {
+    function decode(string memory _geohash)
+        public
+        view
+        returns (int64[2] memory)
+    {
         int64[4] memory bbox = decodeBbox(_geohash);
         int64 minLat = bbox[0];
         int64 minLon = bbox[1];
@@ -238,7 +264,7 @@ contract GeohashRegistry {
         // Get center value of bounding box
         int64 lat = (minLat + maxLat) / 2; // Latitude middle point
         int64 lon = (minLon + maxLon) / 2; // Longitude middle point
-        
+
         return [lat, lon];
     }
 
@@ -249,7 +275,11 @@ contract GeohashRegistry {
      * @return Array of Big Number integer coordinates representing the 
         bounding box (lower left point, upper right point)
      */
-    function decodeBbox(string memory _geohash) private view returns (int64[4] memory) {
+    function decodeBbox(string memory _geohash)
+        private
+        view
+        returns (int64[4] memory)
+    {
         bool isLon = true;
         int64 maxLat = MAX_LAT;
         int64 minLat = MIN_LAT;
@@ -287,5 +317,4 @@ contract GeohashRegistry {
         }
         return [minLat, minLon, maxLat, maxLon];
     }
-    
 }
